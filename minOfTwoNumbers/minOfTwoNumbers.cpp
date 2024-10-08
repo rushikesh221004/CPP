429dd0f85e073d96cf45c5f605aabb1676a05165
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-10-08 22:34:15 +0530

    Minimum of two numbers

---
+++ minOfTwoNumbers/minOfTwoNumbers.cpp
@@ -0,0 +1,13 @@
+// Minimum of two numbers.
+
+#include<iostream>
+
+int minNumber(int a, int b) {
+    return (a<b) ? a : b;
+}
+
+int main() {
+    int minOfTwoNumbers = minNumber(1, 6);
+    std::cout << "Minimum number is = " << minOfTwoNumbers;
+    return 0;
+}
